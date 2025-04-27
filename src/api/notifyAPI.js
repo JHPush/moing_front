6a39a86e1e20806fa70b8d10894f492973e059e4
PoorHotsix <majch98@gmail.com>
2025-04-27 21:14:42 +0900

    Update Last Last Last

--- src/api/notifyAPI.js
+++ src/api/notifyAPI.js
@@ -3,12 +3,12 @@ import axios from "axios"
 const PREFIX_URL = process.env.REACT_APP_PREFIX_URL
 
 export const getNotify = async (userId)=>{
-    const res = await axios.get(`${PREFIX_URL}/notify`, { params: { userId } });
+    const res = await axios.get(`${PREFIX_URL}/moing/notify`, { params: { userId } });
     return res;
 }
 
 export const changeIsRead = async (id, post_id) =>{
-    const res = await axios.post(`${PREFIX_URL}/notify`, {  
+    const res = await axios.post(`${PREFIX_URL}/moing/notify`, {  
         id: id, 
         post_id: post_id 
     } ,{
@@ -26,7 +26,7 @@ export const changeIsRead = async (id, post_id) =>{
 
 
 export const deleteNotify = async (gathering_id, userId, post_id) => {
-  const res = await axios.delete(`${PREFIX_URL}/notify`, {
+  const res = await axios.delete(`${PREFIX_URL}moing/notify`, {
     data: { gathering_id, userId, post_id }, // ← 여기에 data로!
     headers: {
       'Content-Type': 'application/json',
